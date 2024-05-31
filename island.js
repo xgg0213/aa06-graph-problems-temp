f81fa2521a335793ab73ba16590aff1a825941b0
xgg0213 <m.eringan@gmail.com>
2024-05-31 15:00:47 -0700

    getNeighbors completed

--- island.js
+++ island.js
@@ -11,6 +11,24 @@ function getNeighbors(row, col, graph) {
   // Return neighbors
 
   // Your code here 
+  let startNode = [row, col];
+  let up = [row-1, col];
+  let bottom = [row+1, col];
+  let left = [row, col-1];
+  let right = [row, col+1];
+  let check = [up, bottom, left, right]
+  let res = [];
+
+  for (let i = 0; i < check.length; i++) {
+    let el = check[i];
+    if (el[0] >= 0 && el[1] >= 0 && el[0] <= graph.length-1 && el[1] <= graph[0].length-1 && graph[el[0]][el[1]]>=1) {
+        res.push(el);
+    }
+    
+  }
+
+return res;
+  
 }
 
 
@@ -38,6 +56,27 @@ function islandSize(row, col, graph) {
   // return size
 
   // Your code here 
+  
+
 }
 
 module.exports = [getNeighbors, islandSize];
+
+
+matrix = [
+  [1,1,1,0,0],
+  [0,1,1,0,1],
+  [0,1,1,0,1],
+]
+
+matrix2 = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+]
+
+console.log(getNeighbors(1, 1, matrix2))//.to.have.length(0);
+console.log(getNeighbors(1, 1, matrix))//.to.have.deep.members([[0, 1], [1, 2], [2, 1]]);
+console.log(getNeighbors(0, 0, matrix))//.to.have.deep.members([[0, 1]]);
+console.log(getNeighbors(2, 4, matrix))//.to.have.deep.members([[1, 4]]);
+console.log(getNeighbors(1, 0, matrix))//.to.have.deep.members([[0, 0], [1, 1]]);
